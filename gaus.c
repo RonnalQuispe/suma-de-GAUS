35a35e7c2c985945236d17a276bef9707175c275
Laboratorio <laboratorio@udla-ec.int>
2023-10-16 16:33:14 -0500

    segundo commit

--- gaus.c
+++ gaus.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main(void){
+int numero;
+int i;
+int suma=0;
 
+    printf("Ingrese el numero entero:\n");
+    scanf("%d\n", &numero);
+    for (i=1; i <= numero; i++)
+    {
+        suma=suma+i;
+    }
+    
+printf("la sumatoria es:%d\n", suma);
 
     return 0;
 }
